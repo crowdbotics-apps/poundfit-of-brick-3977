fc3ccc89c12769885e3aa585d8a82198ca278d4a
Crowdbotics <team@crowdbotics.com>
2019-05-29 15:27:05 +0000

    initial commit

---
+++ android/app/src/main/java/com/poundfit_of_brick_3977/MainActivity.java
@@ -0,0 +1,15 @@
+package com.poundfit_of_brick_3977;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "poundfit_of_brick_3977";
+    }
+}
